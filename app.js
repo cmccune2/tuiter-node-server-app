b3d9b95d5f7250d22a7c28cea91ab9c4393f0d4c
cmccune2 <98060977+cmccune2@users.noreply.github.com>
2023-04-06 12:56:19 -0400

    a9 working

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ import UserController
     from "./controllers/users/users-controller.js"
 import TuitsController
     from "./controllers/tuits/tuits-controller.js";
+import mongoose from "mongoose";
+
+mongoose.connect('mongodb+srv://mccune124:Cowboy2499@cluster0.igxhyiz.mongodb.net/tuiter?retryWrites=true&w=majority');
 
 
 const app = express()
