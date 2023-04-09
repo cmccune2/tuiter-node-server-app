cd5e352e8e36acaa1c5d5f8c65735db743ae100e
cmccune2 <98060977+cmccune2@users.noreply.github.com>
2023-04-09 18:45:03 -0400

    a9

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
-import express from 'express'
+import express from 'express';
+import mongoose from "mongoose";
 import cors from 'cors'
+import dotenv from "dotenv";
+
 import HelloController
     from "./controllers/hello-controller.js"
 import UserController
     from "./controllers/users/users-controller.js"
 import TuitsController
     from "./controllers/tuits/tuits-controller.js";
-import mongoose from "mongoose";
-
-mongoose.connect('mongodb+srv://mccune124:Cowboy2499@cluster0.igxhyiz.mongodb.net/tuiter?retryWrites=true&w=majority');
 
 
+dotenv.config();
 const app = express()
+
+
+//mongoose.connect('mongodb+srv://mccune124:Cowboy2499@cluster0.igxhyiz.mongodb.net/tuiter?retryWrites=true&w=majority');
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+                          || 'mongodb://127.0.0.1:27017/tuiter';
+
+
+mongoose.connect(CONNECTION_STRING).then(r => console.log('db connection'));
 app.use(express.json())
 app.use(cors())
 TuitsController(app)
